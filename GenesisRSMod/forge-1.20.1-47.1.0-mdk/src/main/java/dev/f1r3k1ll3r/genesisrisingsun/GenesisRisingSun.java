9d15a760e569b93e3915b44f746bec587f9a92b2
Alexandre <alexandrepiresbaptista@gmail.com>
2023-08-02 18:34:40 +0100

    Mod implementation and class creation

---
+++ GenesisRSMod/forge-1.20.1-47.1.0-mdk/src/main/java/dev/f1r3k1ll3r/genesisrisingsun/GenesisRisingSun.java
@@ -0,0 +1,8 @@
+package dev.f1r3k1ll3r.genesisrisingsun;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(GenesisRisingSun.MODID)
+public class GenesisRisingSun {
+    public static final String MODID = "genesisrisingsun";
+}
